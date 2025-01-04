f6b5d153a3012059c989147348e96422e138249e
U-C-S <uchanakyasrinivas@gmail.com>
2025-01-04 22:13:02 +0530

    EOD

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,31 @@
+use regex::Regex;
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
+
+pub fn is_match(haystack: &str, glob: &str) {}
+
+fn convert_to_regex(pattern: &str) {
+    // https://{meow,purr}.cat.com
+    // (meow|purr)\.cat\.com - valid regex
+    let list_regex = Regex::new(r"\{(?<middle>.*)\}").unwrap();
+    let mut lists = vec![];
+
+    for (_, [middle]) in list_regex.captures_iter(pattern).map(|c| c.extract()) {
+
+    }
+}
+
+enum Primitives {
+    Empty,
+    Literal,
+    Any,
+    Single,
+    List,
+    Range,
+}
+
 
 #[cfg(test)]
 mod tests {
